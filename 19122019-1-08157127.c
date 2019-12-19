7f95b833465726546abb035bfd54bc02d0fa26fe
U08157127 <59036802+U08157127@users.noreply.github.com>
2019-12-19 10:42:12 +0800

    Add files via upload

---
+++ 19122019-1-08157127.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int myDec ;
+    printf("please input");
+    scanf("%d", &myDec);
+    printf("%x", myDec);
+
+    return 0;
+}
+
